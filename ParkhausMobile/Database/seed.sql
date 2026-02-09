a75eef844685830fc2036dbc13794e2d2b08fcb3
Romeo D'Addio <romeo.daddio@gmail.com>
2026-02-09 20:36:25 +0100

    add test data for garages

---
+++ ParkhausMobile/Database/seed.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Garages (Name, Address, Capacity, FreeSpaces, OpeningHours, PricingInfo)
+VALUES
+('Parkhaus Centrum', 'Basel, Zentrum', 300, 120, 'Mo-So 00:00-24:00', 'CHF 2.50 / Std.'),
+('Parkhaus Bahnhof', 'Basel SBB', 500, 35, 'Mo-So 00:00-24:00', 'CHF 3.00 / Std.'),
+('Parkhaus Messe', 'Basel Messeplatz', 200, 180, 'Mo-So 06:00-23:00', 'CHF 2.00 / Std.'),
+('Parkhaus City', 'Basel Innenstadt', 150, 10,  NULL,    NULL),
+('Parkhaus St. Jakob', 'Basel St. Jakob', 400, 260, 'Mo-So 00:00-24:00', 'CHF 2.50 / Std.');
