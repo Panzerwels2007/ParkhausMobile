730587b7763f45e31b644209d77f0fdd28703387
Romeo D'Addio <romeo.daddio@gmail.com>
2026-02-10 18:55:07 +0100

    add display dashboard statistics from sql server data

--- ParkhausMobile/Views/DashboardPage.xaml.cs
+++ ParkhausMobile/Views/DashboardPage.xaml.cs
@@ -1,9 +1,30 @@
+using ParkhausMobile.Services;
+using System.Linq;
+
 namespace ParkhausMobile.Views;
 
 public partial class DashboardPage : ContentPage
 {
-	public DashboardPage()
-	{
-		InitializeComponent();
-	}
+    public DashboardPage()
+    {
+        InitializeComponent();
+
+        var service = new GarageService();
+        var garages = service.GetGarages();
+
+        var totalGarages = garages.Count;
+        var totalFreeSpaces = garages.Sum(g => g.FreeSpaces);
+        var totalCapacity = garages.Sum(g => g.Capacity);
+
+        double occupancyPercent = 0;
+        if (totalCapacity > 0)
+        {
+            occupancyPercent = 100 - ((double)totalFreeSpaces / totalCapacity * 100);
+        }
+
+        GaragesCountLabel.Text = $"Parkhäuser: {totalGarages}";
+        FreeSpacesLabel.Text = $"Freie Plätze gesamt: {totalFreeSpaces}";
+        CapacityLabel.Text = $"Gesamtkapazität: {totalCapacity}";
+        OccupancyLabel.Text = $"Auslastung: {occupancyPercent:F1} %";
+    }
 }
