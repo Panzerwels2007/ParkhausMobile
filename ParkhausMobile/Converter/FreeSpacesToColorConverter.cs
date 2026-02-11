e5b1a805ab828161ec50398cffd9dc0810715d4f
Romeo D'Addio <romeo.daddio@gmail.com>
2026-02-11 17:22:38 +0100

    add color status indicator for free spaces

---
+++ ParkhausMobile/Converter/FreeSpacesToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ParkhausMobile.Converters;
+
+public class FreeSpacesToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not int freeSpaces)
+            return Colors.Black;
+
+        if (freeSpaces > 100)
+            return Colors.Green;
+
+        if (freeSpaces > 20)
+            return Colors.Orange;
+
+        return Colors.Red;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
